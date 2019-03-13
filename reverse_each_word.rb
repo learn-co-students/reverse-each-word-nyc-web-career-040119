815dca3524494378cc9bbdb1d19fd79fda71abfd
Edgar Ong <emailedgar@gmail.com>
2019-03-13 19:57:01 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+# def reverse_each_word(string)
+#   string_array = string.split
+#   reverse_string = []
+#   string_array.each do |array|
+#     reverse_string << array.reverse
+#   end
+#   reverse_string.join(" ")
+# end
+
+def reverse_each_word(string)
+  string_array = string.split
+  
+  string_array.collect do |array|
+    array.reverse
+  end.join(" ")
+end
